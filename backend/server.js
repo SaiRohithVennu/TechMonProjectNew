dd4b4f815f80fae2f1c9b9b5d1ab59e79038cee5
Rohith Vennu <rohithvennu@mac.lan>
2025-04-20 12:15:17 -0400

    fix: use MONGODB_URI to match Render environment variable

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use('/api/activities', activityRoutes);
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
