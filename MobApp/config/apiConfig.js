c83011d89e0de1e1f672ac1b136034b6aa3eeaa9
Rohith Vennu <rohithvennu@mac.lan>
2025-04-20 12:28:12 -0400

    fix: updated API base URL to production

--- MobApp/config/apiConfig.js
+++ MobApp/config/apiConfig.js
@@ -1,6 +1,4 @@
 import { Platform } from 'react-native';
 
-export const API_BASE_URL =
-  Platform.OS === 'web'
-    ? 'http://localhost:5000/api'
-    : 'http://192.168.86.89:5000/api';
+export const API_BASE_URL = 'https://techmonprojectnew.onrender.com/api';
+
